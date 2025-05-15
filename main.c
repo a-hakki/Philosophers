a85447f4f72e7079025cace09d2e7c05724251ff
a-hakki <abdelazizhatake@gmail.com>
2025-05-15 18:34:12 +0100

    first philo test

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <pthread.h>
+
+typedef struct s_vars {
+	int philo_n;
+	int sleep_time;
+	int die_time;
+	int eat_time;
+	int boul;
+	pthread_mutex_t mut;
+} t_vars;
+
+void *f1(void *vars)
+{
+	t_vars *args = (t_vars *)vars;
+	int i = 0;
+	while (1)
+	{
+		pthread_mutex_lock(&args->mut);
+		args->eat_time++;
+		pthread_mutex_unlock(&args->mut);
+		
+		i++;
+		if (i == 1000000)
+			break;
+	}
+	return (NULL);
+}
+
+int main(int ac, char **av)
+{
+	t_vars vars;
+	vars.philo_n = 4;
+	vars.sleep_time = 200;
+	vars.die_time = 500;
+	vars.eat_time = 0;
+	vars.boul = 1;
+	pthread_mutex_init(&vars.mut, NULL);
+
+	pthread_t t1, t2;
+	pthread_create(&t1, NULL, f1, &vars);
+	pthread_create(&t2, NULL, f1, &vars);
+
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+
+	printf("%d\n", vars.eat_time);
+	pthread_mutex_destroy(&vars.mut);
+	return 0;
+}
